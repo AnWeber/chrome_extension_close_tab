2353055512209e7a309a382e6b7504d7eee23e23
Andreas Weber <weber.andreas@gmail.com>
2024-03-23 21:47:27 +0100

    feat: add action for icon click

--- background.js
+++ background.js
@@ -2,26 +2,43 @@
 chrome.commands.onCommand.addListener(async (command) => {
     const tabs = await chrome.tabs.query({ currentWindow: true });
     if (command === 'close-origin') {
-        const activeTab = getActiveTab(tabs);
-        const origin = getOriginOfTab(activeTab);
-        await closeTabsWithPredicate(tabs, t => getOriginOfTab(t) === origin);
+        await closeSameOriginTabs(tabs);
     }
     if (command === 'close-duplicate') {
-        const activeTab = getActiveTab(tabs);
-        await closeTabsWithPredicate(tabs, t => t.id !== activeTab.id && t.url === activeTab.url);
+        await closeDuplicateTabs(tabs);
     }
     if (command === 'close-all-duplicate') {
-        let cache = {};
-        await closeTabsWithPredicate(tabs, t => {
-            const url = t.url;
-            if (cache[url]) {
-                return true;
-            }
-            cache[url] = 1;
-            return false;
-        });
+        await closeAllDuplicateTabs(tabs);
     }
 });
+
+chrome.action.onClicked.addListener(async function (tab) {
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+    await closeAllDuplicateTabs(tabs);
+});
+
+async function closeSameOriginTabs(tabs) {
+    const activeTab = getActiveTab(tabs);
+    const origin = getOriginOfTab(activeTab);
+    await closeTabsWithPredicate(tabs, t => getOriginOfTab(t) === origin);
+}
+
+async function closeDuplicateTabs(tabs) {
+    const activeTab = getActiveTab(tabs);
+    await closeTabsWithPredicate(tabs, t => t.id !== activeTab.id && t.url === activeTab.url);
+}
+
+async function closeAllDuplicateTabs(tabs) {
+    let cache = {};
+    await closeTabsWithPredicate(tabs, t => {
+        const url = t.url;
+        if (cache[url]) {
+            return true;
+        }
+        cache[url] = 1;
+        return false;
+    });
+}
 
 async function closeTabsWithPredicate(tabs, predicate) {
     for (const tab of tabs) {
